8ebeefe90b32ee6e1b2089b1f436a97ebdf93edc
iam-dante <koreanhoseon@gmail.com>
2020-03-08 01:48:52 +0900

    Sharings model 생성 전 커밋

--- sharings/models.py
+++ sharings/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Sharings(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True) # 해당 레코드 생성 시 현재 시간 자동저장
+    til = models.TextField() # 길이 제한 없이 생성
+    action_plan = models.TextField() # 길이 제한 없이 생성
