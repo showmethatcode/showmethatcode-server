1aa1f7d686a7129d59b25b30a223c63d86a62c69
iam-dante <koreanhoseon@gmail.com>
2020-03-08 20:38:43 +0900

    디테일 페이지 id값 줌(임시)

--- sharings/views.py
+++ sharings/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 from django.http import HttpResponse
 from sharings.models import Sharings
+from django.urls import reverse
+
 
 # views = 비지니스 로직
 
@@ -12,3 +14,7 @@ def detail(request, sharing_id):
     return render(request, 'detail.html', {
         'id': sharing_id,
     })
+
+
+def detail_temp(request):
+    return HttpResponseRedirect('/detail/1')
