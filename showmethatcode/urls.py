e15c0f1dc39da999b82aafa03e213b01616c9820
iam-dante <koreanhoseon@gmail.com>
2020-03-08 18:16:12 +0900

    템플릿 연동 기초작업

--- showmethatcode/urls.py
+++ showmethatcode/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from sharings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sharings/', include('sharings.urls'))
+    path('', views.sharings, name='sharings'),
+    path('detail/<int:sharing_id>/', views.detail, name='detail'),
 ]
