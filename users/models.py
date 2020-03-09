9e375ec1eaea2e6ca867e2a280337fec88c2ba0b
well-balanced <ws.kim@specupad.com>
2020-03-10 01:56:23 +0900

    Add team members

--- users/models.py
+++ users/models.py
@@ -5,6 +5,8 @@ class UserManager(UserManager):
     def create_user(self, *args, **kwargs):
         team_members = [
             'softkey95@gmail.com',
+            'incleaf@gmail.com',
+            'koreanhoseon@gmail.com',
         ]
         kwargs['is_team_member'] = True if kwargs['email'] in team_members else False
         print(kwargs['is_team_member'])
@@ -16,4 +18,3 @@ class User(AbstractBaseUser):
 
     objects = UserManager()
     USERNAME_FIELD = 'email'
-# "password" varchar(128) NOT NULL, "last_login" datetime NULL, "is_superuser" bool NOT NULL, "username" varchar(150) NOT NULL UNIQUE, "first_name" varchar(30) NOT NULL, "email" varchar(254) NOT NULL, "is_staff" bool NOT NULL, "is_active" bool NOT NULL, "date_joined" datetime NOT NULL, "last_name" varchar(150) NOT NULL)
