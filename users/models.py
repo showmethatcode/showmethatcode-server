4aa5008dca89c64a0879f08b57edb77cb82b8a72
iam-dante <koreanhoseon@gmail.com>
2020-03-25 04:22:32 +0900

    프로필 이미지

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.templatetags.static import static
 
 class UserManager(UserManager):
     def create_user(self, *args, **kwargs):
@@ -7,6 +8,7 @@ class UserManager(UserManager):
             'softkey95@gmail.com',
             'incleaf@gmail.com',
             'koreanhoseon@gmail.com',
+            'qpwpep5429@gmail.com'
         ]
         kwargs['is_team_member'] = True if kwargs['email'] in team_members else False
         print(kwargs['is_team_member'])
@@ -15,6 +17,17 @@ class UserManager(UserManager):
 class User(AbstractBaseUser):
     email = models.TextField(unique=True, verbose_name='유저아이디')
     is_team_member = models.BooleanField(verbose_name='팀 멤버 유무', default=False)
-
+    
     objects = UserManager()
     USERNAME_FIELD = 'email'
+    
+    def user_profile(self):
+        get_user_profile = {
+            'softkey95@gmail.com': static('images/woosik.png'),
+            'incleaf@gmail.com': static('images/hyeonsu.png'),
+            'koreanhoseon@gmail.com': static('images/hoseon.png'),
+            'qpwpep5429@gmail.com': static('images/sangmin.png'),
+        }
+        return get_user_profile[self.email]
+
+
