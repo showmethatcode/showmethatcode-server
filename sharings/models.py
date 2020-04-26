22d6698656090e480c61f56e5a0bcc4e8d8f73e9
iam-dante <koreanhoseon@gmail.com>
2020-04-26 22:17:51 +0900

    비밀글 기능 추가

--- sharings/models.py
+++ sharings/models.py
@@ -12,3 +12,4 @@ class Sharing(models.Model):
     til = models.TextField() # 길이 제한 없이 생성
     action_plan = models.TextField() # 길이 제한 없이 생성
     created_at = models.DateTimeField(auto_now_add=True) # 해당 레코드 생성 시 현재 시간 자동저장
+    is_secret = models.NullBooleanField()
